434dfcb43b35edc3ceeccaf9879e6a9550476cc0
Luis Alberto Jimenez Aponte <luis.jimenez@datco.cl>
2020-04-07 17:13:14 -0400

    descripcion

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,255 @@
+import { Component } from '@angular/core';
+import { StorageService ,Des} from '../services/storage.service';
+import { Platform } from '@ionic/angular';
+export const coordenadasCarX = 469;
+export const coordenadasCarY = 50;
+export const token = 6;
+var canvas: any;
+var canvasa: any;
+var iluminacion = 1;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  items:Des[] = []
+  est:any = 0;
+
+  constructor(private storageService:StorageService,private plt: Platform) {
+    this.plt.ready().then(() => {
+      this.loadItems();
+    });
+  }
+
+  loadItems() {
+    this.storageService.addItem().then(items =>{
+      this.items = items;
+      let itemCoordenada = this.items.filter(coor => coor.id == token);
+      this.est = itemCoordenada[0].nombre;
+    })
+  }
+
+  getItem() {
+    this.storageService.loadItem();
+  }  
+
+  /*getCoordinates(event){
+      let Y =(event.offsetY + 15);
+      let X = (event.offsetX + 5);   
+      console.log(Y);
+      console.log(X);
+    }*/
+
+    redirect(){
+      location.reload();
+    }
+
+
+  SetRuta()
+  {
+
+  let itemCoordenada = this.items.filter(coor => coor.id == token);
+  draw(itemCoordenada)
+
+  setInterval(function () {
+    SetRutaAgain(itemCoordenada);
+  }, 2000);
+
+  function draw(ite) {    
+
+    if (document.getElementById("video")) {
+      document.getElementById("video").remove();
+    }
+
+    let elm = document.getElementById('auto');
+    elm.setAttribute("style", "display:block");
+
+    elm = document.getElementById('myCanvas');
+    elm.setAttribute("style","display:block");
+
+    elm = document.getElementById('footer');
+    elm.setAttribute("style","display:block");
+
+    elm = document.getElementById('header');
+    elm.setAttribute("style","display:block");
+
+    elm = document.getElementById('content');
+    elm.setAttribute("style","display:none");
+
+    elm = document.getElementById('fixed');
+    elm.setAttribute("style","position: relative;height: 90%;width: 100%;overflow: scroll;");
+
+    let coordenadaX = coordenadasCarX;
+    let coordenadaY = coordenadasCarY;
+
+    let coordenadas = ite[0].coordenadas;
+
+    let arrCoordenadas = coordenadas.split(',');
+
+     canvas = document.getElementById("myCanvas")
+     var ctx = canvas.getContext("2d");
+     ctx.clearRect(0, 0, 800, 500);
+     ctx.beginPath();
+     ctx.moveTo(coordenadasCarX, coordenadasCarY);  
+     ctx.lineWidth = 4;
+     ctx.strokeStyle = "rgba(47,71,147,1)";
+
+     for(var ar in arrCoordenadas){
+        var jsonCoordenadas = JSON.parse(arrCoordenadas[ar])
+
+        if(jsonCoordenadas.hasOwnProperty("x")){
+          if(coordenadaX > jsonCoordenadas.x){
+            for (var i = coordenadaX; i >= jsonCoordenadas.x; i--){
+              doSetTimeoutXNegativo(i,coordenadaY)
+            }
+            coordenadaX = jsonCoordenadas.x;
+          }else{
+            for (var i = coordenadaX; i <= jsonCoordenadas.x; i++){
+              doSetTimeoutXPositivo(i,coordenadaY);
+            }
+            coordenadaX = jsonCoordenadas.x;
+          }
+        }else{
+          if(coordenadaY > jsonCoordenadas.y){
+            for (var i = coordenadaY; i >= jsonCoordenadas.y; i--){
+              doSetTimeoutYNegativo(coordenadaX,i)
+            }
+            coordenadaY = jsonCoordenadas.y;
+          }else{
+            for (var i = coordenadaY; i <= jsonCoordenadas.y; i++){
+              doSetTimeoutYPositivo(coordenadaX,i);
+            }
+            coordenadaY = jsonCoordenadas.y;
+          }
+        }
+     }
+     
+     ctx.closePath();
+
+     function doSetTimeoutXPositivo(x,y) {
+      setTimeout(function () {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+      }, 2000);
+    }
+
+    function doSetTimeoutXNegativo(x,y) {
+      setTimeout(function () {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+      }, 2000);
+    }
+
+    function doSetTimeoutYPositivo(x,y) {
+      setTimeout(function() { 
+        ctx.lineTo(x, y);
+        ctx.stroke();
+       }, 2000);
+    }
+
+    function doSetTimeoutYNegativo(x,y) {
+      setTimeout(function() { 
+        ctx.lineTo(x, y);
+        ctx.stroke();
+       }, 2000);
+    }
+  }
+
+    function SetRutaAgain(ite)
+    {
+      let itemCoordenada = ite;
+      let coordenadaX = coordenadasCarX;
+      let coordenadaY = coordenadasCarY;
+      let coordenadas = itemCoordenada[0].coordenadas;
+  
+      let arrCoordenadas = coordenadas.split(',');
+  
+       canvasa = document.getElementById("myCanvas")
+       var ctxa = canvasa.getContext("2d");
+       ctxa.beginPath();
+       ctxa.moveTo(coordenadasCarX, coordenadasCarY);  
+       ctxa.lineWidth = 4;
+       ctxa.strokeStyle = "rgba(47,71,147,1)";
+
+       if(iluminacion === 0){
+        ctxa.shadowBlur = 2;
+        ctxa.shadowColor = "#c4a300";
+        iluminacion = 1;
+       } else{
+        ctxa.shadowBlur = 3;
+        ctxa.shadowColor = "#fff";
+        iluminacion = 0;
+       }
+  
+       for(var ar in arrCoordenadas){
+          var jsonCoordenadas = JSON.parse(arrCoordenadas[ar])
+  
+          if(jsonCoordenadas.hasOwnProperty("x")){
+            if(coordenadaX > jsonCoordenadas.x){
+              for (var i = coordenadaX; i >= jsonCoordenadas.x; i--){
+                doSetTimeoutXNegativo(i,coordenadaY)
+              }
+              coordenadaX = jsonCoordenadas.x;
+            }else{
+              for (var i = coordenadaX; i <= jsonCoordenadas.x; i++){
+                doSetTimeoutXPositivo(i,coordenadaY);
+              }
+              coordenadaX = jsonCoordenadas.x;
+            }
+          }else{
+            if(coordenadaY > jsonCoordenadas.y){
+              for (var i = coordenadaY; i >= jsonCoordenadas.y; i--){
+                doSetTimeoutYNegativo(coordenadaX,i)
+              }
+              coordenadaY = jsonCoordenadas.y;
+            }else{
+              for (var i = coordenadaY; i <= jsonCoordenadas.y; i++){
+                doSetTimeoutYPositivo(coordenadaX,i);
+              }
+              coordenadaY = jsonCoordenadas.y;
+            }
+          }
+       }
+  
+       ctxa.closePath();
+  
+       function doSetTimeoutXPositivo(x,y) {
+        setTimeout(function () {
+          ctxa.lineTo(x, y);
+          //ctxa.setLineDash([2, 2]);
+          ctxa.stroke();
+        }, 2000);
+      }
+      
+  
+      function doSetTimeoutXNegativo(x,y) {
+        setTimeout(function () {
+          ctxa.lineTo(x, y);
+          //ctxa.setLineDash([2, 2]);
+          ctxa.stroke();
+        }, 2000);
+      }
+  
+      function doSetTimeoutYPositivo(x,y) {
+        setTimeout(function() { 
+          ctxa.lineTo(x, y);
+          //ctxa.setLineDash([2, 2]);
+          ctxa.stroke();
+         }, 2000);
+      }
+  
+      function doSetTimeoutYNegativo(x,y) {
+        setTimeout(function() { 
+          ctxa.lineTo(x, y);
+          //ctxa.setLineDash([2, 2]);
+          ctxa.stroke();
+         }, 2000);
+      }
+    }
+  } 
+}
+
+
---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+const ITEMS_KEY = 'my-items';
+
+export interface Des {
+  id: number,
+  nombre: string,
+  coordenadas:string,
+  mapa:string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+constructor(private storage: Storage) { }
+
+addItem(): Promise<Des[]>{
+  ///conecta api o servicio
+  let destino = [];
+  destino.push({
+    id: 6,
+    nombre: 'PARKING - 6',
+    coordenadas: '{"x" : 192},{"y" : 163},{"x" : 163}',
+    mapa:'../../../src/assets/img/ParkingDatco.png'
+  },{
+    id: 42,
+    nombre: 'PARKING - 42',
+    coordenadas: '{"x" : 320},{"y" : 113},{"x" : 350}',
+    mapa:'../../../src/assets/img/ParkingDatco.png'
+  });
+  this.storage.set(ITEMS_KEY,destino);
+  return this.storage.get(ITEMS_KEY);
+}
+
+loadItem(): Promise<Des[]>{
+  return this.storage.get(ITEMS_KEY);
+}
+};
